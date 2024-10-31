77d71e28e3f4faa4a63affacfd33be81877a0624
jeffersonrolino <jefferson.rolino@gmail.com>
2024-10-31 06:23:06 -0300

    Capítulo 04 - Aula 05: Verificar tamanho da lista

--- js/adicionarItem.js
+++ js/adicionarItem.js
@@ -1,11 +1,13 @@
 import { criarItemDaLista } from "./criarItemDaLista.js";
+import { verificarListaVazia } from "./verificarListaVazia.js";
 
 const item = document.getElementById("input-item");
-const listaContainer = document.getElementById("lista-container");
+const listaDeCompras = document.getElementById("lista-de-compras");
 
 export function adicionarItem(event) {
   event.preventDefault();
 
   const itemDaLista = criarItemDaLista(item.value);
-  listaContainer.appendChild(itemDaLista);
+  listaDeCompras.appendChild(itemDaLista);
+  verificarListaVazia(listaDeCompras);
 }
